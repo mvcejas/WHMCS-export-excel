84a4f191813202da3ef7be27510320e3425cb0d2
Mar C <mar@mvcejas.com>
2013-10-04 00:49:30 +0800

    few updates

--- admin/exceldownload.php
+++ admin/exceldownload.php
@@ -5,15 +5,16 @@ require realpath(dirname(__FILE__)) . '/../dbconnect.php';
 // Create new PHPExcel object
 $objPHPExcel = new PHPExcel();
 
-// Set document properties
-$objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
-							 ->setLastModifiedBy("Maarten Balliauw")
-							 ->setTitle("Office 2007 XLSX Test Document")
-							 ->setSubject("Office 2007 XLSX Test Document")
-							 ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
-							 ->setKeywords("office 2007 openxml php")
-							 ->setCategory("Test result file");
+// set document orientation
+$objPHPExcel->getActiveSheet()
+			->getPageSetup()
+			->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
 
+// set default font
+$objPHPExcel->getDefaultStyle()
+			->getFont()
+		    ->setName('Arial')
+		    ->setSize(10);
 
 // create headers
 $objPHPExcel->setActiveSheetIndex(0)
@@ -22,6 +23,13 @@ $objPHPExcel->setActiveSheetIndex(0)
 			->mergeCells('C1:C2')
 			->mergeCells('D1:D2');
 
+
+// set width to column A
+$objPHPExcel->setActiveSheetIndex(0)
+			->getColumnDimension('A')
+			->setWidth(30);
+
+// set headers text
 $objPHPExcel->setActiveSheetIndex(0)
 			->setCellValue('E1', 'CASSA __ "c"')
 			->setCellValue('G1', 'BANCA __ "b"')
@@ -42,33 +50,57 @@ $objPHPExcel->setActiveSheetIndex(0)
 $objPHPExcel->setActiveSheetIndex(0)
 			->mergeCells('E1:F1')
 			->getStyle('E1:F1')
-			->getAlignment()
-    		->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+    		->applyFromArray(array(
+                'font' => array(
+                	'size' => 12,
+            	),
+	        	'alignment' => array(
+			        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+			    ),
+			    'borders' => array(
+			    	'color' => array('rgb','000000'),
+			    	'style' => 'solid'
+			    ),
+            ));
 
 $objPHPExcel->setActiveSheetIndex(0)
 			->mergeCells('G1:H1')
 			->getStyle('G1:H1')
-			->getAlignment()
-    		->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-
+			->applyFromArray(array(
+                'font' => array(
+                	'size' => 12,
+            	),
+	        	'alignment' => array(
+			        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+			    ),
+            ));
 $objPHPExcel->setActiveSheetIndex(0)
 			->mergeCells('I1:K1')
 			->getStyle('I1:K1')
-			->getAlignment()
-    		->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+			->applyFromArray(array(
+                'font' => array(
+                	'size' => 12,
+            	),
+	        	'alignment' => array(
+			        'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+			    ),
+            ));
 
 $objPHPExcel->setActiveSheetIndex(0)
  			->getStyle('A1:K2')
  			->applyFromArray(array(
 				'fill' => array(
                     'type' => PHPExcel_Style_Fill::FILL_SOLID,
-                    'color' => array('rgb'=>'ffa07a'),
+                    'color' => array('rgb'=>'ffcc99'),
                 ),
                 'font' => array(
-                	'bold' => true
+                	'bold' => true,
+                	'color' => array('rgb'=>'993314')
             	)
             ));
 
+// the next row count after the headers is
+$row = 3;
 
 // Rename worksheet
 $objPHPExcel->getActiveSheet()->setTitle('Sheet Title');
@@ -81,12 +113,11 @@ if(isset($_POST) && count($_POST)){
 	$datefrom = strtotime($_POST['datefrom']);
 	$dateto   = strtotime($_POST['dateto']);
 
-	$query = "SELECT 
-		CONCAT(t2.firstname,0x20,t2.lastname) AS description
+	$query = "SELECT t2.lastname AS description
 		, t1.invoicenum
 		, t1.total
 		, IF(t3.transid!='','P','B') AS method
-		FROM whmcs.tblinvoices t1 
+		FROM tblinvoices t1 
 		JOIN tblclients t2 ON t1.userid=t2.id
 		JOIN tblaccounts t3 ON t1.id=t3.invoiceid
 		WHERE t1.status='Paid' AND t1.datepaid BETWEEN $datefrom AND $dateto
@@ -96,17 +127,31 @@ if(isset($_POST) && count($_POST)){
 
 	$worksheet = $objPHPExcel->getActiveSheet();
 
-	$row = 3;
 	while($obj = mysql_fetch_object($result)){
 		$worksheet->setCellValue('A'.$row, $obj->description)
-				  ->setCellValue('B'.$row, $obj->invoicenum)
-				  ->setCellValue('C'.$row, $obj->total)
+				  ->setCellValue('B'.$row, preg_replace("/ITH2013-/","",$obj->invoicenum))
+				  ->setCellValue('C'.$row, number_format($obj->total,2))
 				  ->setCellValue('D'.$row, $obj->method)
 				  ->setCellValue('E'.$row, '=IF(D'.$row.'="C",C'.$row.',"")')
 				  ->setCellValue('G'.$row, '=IF(D'.$row.'="B",C'.$row.',"")')
-				  ->setCellValue('I'.$row, '=IF(D'.$row.'="P",SUM((C'.$row.'*4/100)+C'.$row.'+0.34),"")');
+				  ->setCellValue('I'.$row, '=IF(D'.$row.'="P",SUM((C'.$row.'*4/100)+C'.$row.'+0.34),"")')
+				  ->setCellValue('J'.$row, '=IF(D'.$row.'=IF(D'.$row.'="p",SUM(I'.$row.'-(I'.$row.'*2.7/100)-0.35),"")');
 	 	$row++;
 	}
+}
+
+$styleArray = array(
+       'borders' => array(
+             'outline' => array(
+                    'style' => PHPExcel_Style_Border::BORDER_THIN,
+                    'color' => array('rgb' => '0000FF'),
+             ),
+       ),
+);
+foreach(range(1,$row) as $r){
+	$objPHPExcel->getActiveSheet()
+				->getStyle('A'.$row.':K'.$row)
+				->applyFromArray($styleArray);
 }
 
 // Redirect output to a client’s web browser (Excel5)
